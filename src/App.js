d45b69700e15b6be103935ed97f003439688d31c
AugustoV2 <augustovarghese@gmail.com>
2024-08-15 00:06:13 +0530

    jgygb

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Home from './components/home';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './login';
+import Login from './components/login';
 
 function App() {
   return (
