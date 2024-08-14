8fdea2ec536885af3842ea687cd61cbb013a19a2
AugustoV2 <augustovarghese@gmail.com>
2024-08-14 23:57:00 +0530

    augustologin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    
+    './src//*.{js,jsx,ts,tsx}',
+     'node_modules/flowbite-react/lib/esm//*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+
+  ],
 }
